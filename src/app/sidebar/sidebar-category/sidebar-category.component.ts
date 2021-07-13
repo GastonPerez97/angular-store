398a00b12c57832fdd0f3d15217f8a49b9eb1c55
Gaston Perez <gastonperez-97@hotmail.com>
2021-07-13 18:27:33 -0300

    modified sidebar of productos

---
+++ src/app/sidebar/sidebar-category/sidebar-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-category',
+  templateUrl: './sidebar-category.component.html',
+  styleUrls: ['./sidebar-category.component.css']
+})
+export class SidebarCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
