c2d3cb9fa0ee210e1d8ac2427a4b3887e4f0a789
Gaston Perez <gastonperez-97@hotmail.com>
2021-07-13 19:03:43 -0300

    added add to cart in precio producto component. fixed detalle views

--- src/app/detalle-producto/detalle-producto.component.ts
+++ src/app/detalle-producto/detalle-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CarritoService, Product } from '../services/carrito.service';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalle-producto.component.css']
 })
 export class DetalleProductoComponent implements OnInit {
+    product: Product;
 
-  constructor() { }
+    constructor(private carritoService: CarritoService, private router: Router) {
+        this.product = {
+            id: 1,
+            name: "titulo hardcodeado",
+            category: "categoria hardcodeada",
+            unitPrice: 1337,
+            quantity: 1,
+            totalPrice: 0
+        }
+        
+        this.product.totalPrice = this.product.unitPrice;
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
