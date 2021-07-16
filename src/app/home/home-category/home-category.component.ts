24f26d189b0890b43520c9471da7d2ac62c0f469
ldinubila <lucas.dinubila@gmail.com>
2021-07-15 22:42:05 -0300

    Se muestran las categorias en la home

--- src/app/home/home-category/home-category.component.ts
+++ src/app/home/home-category/home-category.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from "../../interfaces/Product";
 
 @Component({
   selector: 'app-home-category',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-category.component.css']
 })
 export class HomeCategoryComponent implements OnInit {
+  @Input()
+  producto: Product;
 
-  constructor() { }
+
+  constructor() {
+    this.producto = {
+      id: 0,
+      name: "",
+      category: "",
+      price: 0,
+      description: "",
+      urlImage: "",
+      quantity: 0,
+      totalPrice: 0
+  }
+   }
 
   ngOnInit(): void {
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../interfaces/Product';
+import { RestService } from '../services/rest.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  productos: Product[];
+  categoria: any;
 
-  constructor() { }
+  constructor(private RestService: RestService, private _route: ActivatedRoute) {
+    this.productos = [];
+}
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+    this._route.paramMap.subscribe(params => {
+        this.categoria = params.get('category');
+        this.categoria != null ? this.getProductsByCategory(this.categoria) : this.getAllProducts();
+    });
+}
+
+public getAllProducts() {
+    this.RestService.get('/taller-web-2/api/products')
+    .subscribe(productos => {
+        this.productos = productos;
+    });
+}
+
+public getProductsByCategory(category: any) {
+    this.RestService.get(`/taller-web-2/api/products/${category}`)
+    .subscribe(productos => {
+        this.productos = productos;
+    });
+}
 
 }
