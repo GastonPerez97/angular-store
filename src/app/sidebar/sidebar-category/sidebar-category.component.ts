c4c34c598ebe5b2a9e3b52d54f71b59134a88c1a
ldinubila <lucas.dinubila@gmail.com>
2021-07-16 22:36:37 -0300

    Se trae el listado de categorias en productos desde BD

--- src/app/sidebar/sidebar-category/sidebar-category.component.ts
+++ src/app/sidebar/sidebar-category/sidebar-category.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-category',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar-category.component.css']
 })
 export class SidebarCategoryComponent implements OnInit {
+  @Input()
+  categoria: string[];
 
-    constructor() { }
+  constructor() {
+    this.categoria = []
+   }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
 }
