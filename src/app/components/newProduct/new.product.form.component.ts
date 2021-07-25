d6ab2b122fddfe6b88484579d9a6fb82ab23e512
Gaston Perez <gastonperez-97@hotmail.com>
2021-07-25 15:31:50 -0300

    fixed newProduct

--- src/app/components/newProduct/new.product.form.component.ts
+++ src/app/components/newProduct/new.product.form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../../services/categories.service';
 import { ProductsService } from '../../services/product.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/app/interfaces/category';
 
 @Component({
   selector: 'app-new-product-form',
@@ -9,16 +10,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./new.product.component.css']
 })
 export class NewProductFormComponent implements OnInit {
-    categories: string[] = [];
+    categories: Category[] = [];
     checkoutForm: FormGroup;
     message: string;
     error: string;
 
     constructor(private productService: ProductsService, private formBuilder: FormBuilder, private categoriesService: CategoriesService) {
         this.checkoutForm = this.formBuilder.group({
-            id: [0, [Validators.required, Validators.pattern('[1-9][0-9]*')]],
             name: ['', Validators.required],
-            price: [0, [Validators.required, Validators.pattern('[1-9][0-9]*')]],
+            price: [0, [Validators.required, Validators.min(0), Validators.pattern('[1-9][0-9]*')]],
             category: ['Seleccionar', [Validators.required, Validators.pattern('^(?!.*Seleccionar).*$')]],
             description: ['', Validators.required],
             urlImage: ['', [Validators.required, Validators.pattern('(http(s?):\/\/.*\.(?:png|jpg))')]],
@@ -42,9 +42,10 @@ export class NewProductFormComponent implements OnInit {
     public onSubmit() {
         this.message = '';
         this.error = '';
+
         this.productService.newProduct(this.checkoutForm.value).subscribe(
             (response) => this.message = response,
-            (error) => this.error = error,
+            (error) => this.error = error
         )
     }
 
