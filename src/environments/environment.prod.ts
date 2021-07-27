e8abf1b34c2448d61aa77be8c782e33eb0b935e3
Gaston Perez <gastonperez-97@hotmail.com>
2021-07-26 22:31:47 -0300

    fixes for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   	production: true,
-	admin: `${process.env.ADMIN}`,
+	admin: "tallerweb2proyecto@gmail.com",
 
 	firebaseConfig : {
-		apiKey: `${process.env.FIREBASE_API_KEY}`,
-		authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-		projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-		storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-		messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
-		appId: `${process.env.FIREBASE_APP_ID}`
+		apiKey: "AIzaSyDV1EvIscdIVoBIZQab67jQT7EJYZxCuIA",
+		authDomain: "taller-web-2.firebaseapp.com",
+		projectId: "taller-web-2",
+		storageBucket: "taller-web-2.appspot.com",
+		messagingSenderId: "377048704962",
+		appId: "1:377048704962:web:8893570d9465de635a985b"
 	}
 };
