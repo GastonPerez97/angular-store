91c1d88e747fe1ce4057232d1991b571da1e8890
Gastón Pérez <gastonperez97@gmail.com>
2024-01-13 22:31:43 -0300

    use base api url value in product and categories services

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 import { RestService } from './rest.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoriesService {
+  constructor(private RestService: RestService) {}
 
-    constructor(private RestService: RestService) { }
-
-    public getAllCategories() {
-        return this.RestService.get('https://angular-store-api.herokuapp.com/taller-web-2/api/categories');
-    }
+  public getAllCategories() {
+    return this.RestService.get(`${environment.baseApiUrl}/categories`);
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 import { RestService } from './rest.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private restService: RestService) {}
 
-    constructor(private restService: RestService) {}
+  public getAllProducts() {
+    return this.restService.get(`${environment.baseApiUrl}/products`);
+  }
 
-    public getAllProducts() {
-        return this.restService.get('https://angular-store-api.herokuapp.com/taller-web-2/api/products');
-    }
+  public getProductsByCategory(category: any) {
+    return this.restService.get(
+      `${environment.baseApiUrl}/products/${category}`
+    );
+  }
 
-    public getProductsByCategory(category: any) {
-        return this.restService.get(`https://angular-store-api.herokuapp.com/taller-web-2/api/products/${category}`);
-    }
+  public getProductById(id: string) {
+    return this.restService.get(`${environment.baseApiUrl}/product/${id}`);
+  }
 
-	public getProductById(id: string) {
-		return this.restService.get(`https://angular-store-api.herokuapp.com/taller-web-2/api/product/${id}`);
-	}
-
-    public newProduct(product: any) {
-        return this.restService.post('https://angular-store-api.herokuapp.com/taller-web-2/api/product', product);
-    }
+  public newProduct(product: any) {
+    return this.restService.post(`${environment.baseApiUrl}/product`, product);
+  }
 }
